77cc1a9b5cf45126c816d2e88cce3fcd3ecddb5d
Norman Johnson <norman-johnson@lambdastudents.com>
2020-08-14 15:35:57 -0500

    task 2 update

--- index.js
+++ index.js
@@ -221,12 +221,12 @@ console.log(artists[2].bio)
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 console.log('***************TASK 2*******************')
 
-//  function changeName(artists, index, name){
+ function changeName(artists, index, newName){
    
-//   artists[index].name = name;
-//    return artists;
-//  }
-//  console.log(changeName(name, 8, 'Vincent Van Gogh'));
+  artists[index].name = newName;
+   return artists;
+ }
+ console.log(changeName(artists, 8, 'Vincent Van Gogh'));
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
